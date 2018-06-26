004d2a1ea638d5aed1c228ff564920ae3d45b879
pratikadarsh <pratikadarsh24@gmail.com>
2018-06-26 11:23:38 +0530

    Changes to Model and Hyperparameter.

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # devnagiri-character-recognition
 Character recognition for the Devnagiri character dataset.
 Dataset can be downloaded from https://web.archive.org/web/20160105230017/http://cvresearchnepal.com/wordpress/dhcd/
+
+---------------------------
+        ARCHITECTURE
+---------------------------
+
+Model Used: CNN
+Layers:     4
+FC Layers:  2
+Dropout:    Yes(0.4)
+
+---------------------------
+        HYPERPARAMETERS
+---------------------------
+
+Learning Rate: 1E-3
+Epochs:        10
+Batch Size:    100
+
+---------------------------
+       EVALUATION
+---------------------------
+
+Accuracy:       98.53%
+Training Loss:  0.02
+Eval Loss:      0.04
+ 
+
+
--- src/dev.py
+++ src/dev.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-learning_rate = 0.01
+learning_rate = 1E-3
 batch_size = 100
-num_steps = 1000
+num_steps = 10000
 
 def load_data():
     """ Reads the data.csv file to load the images and labels."""
@@ -17,8 +17,8 @@ def load_data():
 
     images = X.values.reshape(X.shape[0], 32,32)
     labels = pd.factorize(Y)[0]
-    dev = {"train_images" : images[:9000],
-           "train_labels" : labels[:9000],
+    dev = {"train_images" : images[:90000],
+           "train_labels" : labels[:90000],
            "test_images"  : images[:-2000],
            "test_labels"  : labels[:-2000]}
     return dev
@@ -26,24 +26,30 @@ def load_data():
 def cnn(x_dict):
     """ Generates the CNN model."""
 
-    x = x_dict['images']
-    x = tf.reshape(x, (-1,32,32,1))
-    x = tf.cast(x, dtype=tf.float32)
-    conv1 = tf.layers.conv2d(inputs=x, filters=16,
-        kernel_size=[5,5], padding="same", activation=tf.nn.relu)
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2)
-    conv2 = tf.layers.conv2d(inputs=pool1, filters=32,
-        kernel_size=[5,5], padding="valid", activation=tf.nn.relu)
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
-    conv3 = tf.layers.conv2d(inputs=pool2, filters=64,
-        kernel_size=[5,5], padding="same", activation=tf.nn.relu)
-    pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2,2], strides=1)
-    conv4 = tf.layers.conv2d(inputs=pool3, filters=128,
-        kernel_size=[5,5], padding="same", activation=tf.nn.relu)
-    pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2,2], strides=1)
-    pool4_flat = tf.reshape(pool4, (-1, 4*4*128))
-    fc1 = tf.layers.dense(inputs=pool4_flat, units=1024, activation=tf.nn.relu)
-    fc2 = tf.layers.dense(inputs=fc1, units=512, activation=tf.nn.relu)
+    with tf.variable_scope("Input_Layer"):
+        x = x_dict['images']
+        x = tf.reshape(x, (-1,32,32,1))
+        x = tf.cast(x, dtype=tf.float32)
+    with tf.variable_scope("First_Layer"):
+        conv1 = tf.layers.conv2d(inputs=x, filters=16,
+            kernel_size=[5,5], padding="same", activation=tf.nn.relu)
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2)
+    with tf.variable_scope("Second_Layer"):
+        conv2 = tf.layers.conv2d(inputs=pool1, filters=32,
+            kernel_size=[5,5], padding="valid", activation=tf.nn.relu)
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
+    with tf.variable_scope("Third_Layer"):
+        conv3 = tf.layers.conv2d(inputs=pool2, filters=64,
+            kernel_size=[5,5], padding="same", activation=tf.nn.relu)
+        pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2,2], strides=1)
+    with tf.variable_scope("Fourth_Layer"):
+        conv4 = tf.layers.conv2d(inputs=pool3, filters=128,
+            kernel_size=[5,5], padding="same", activation=tf.nn.relu)
+        pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2,2], strides=1)
+    with tf.variable_scope("Fully_Connected_Layers"):
+        pool4_flat = tf.reshape(pool4, (-1, 4*4*128))
+        fc1 = tf.layers.dense(inputs=pool4_flat, units=1024, activation=tf.nn.relu)
+        fc2 = tf.layers.dense(inputs=fc1, units=512, activation=tf.nn.relu)
     dropout = tf.layers.dropout(inputs=fc2, rate=0.4)
     output_layer = tf.layers.dense(inputs=dropout, units=56)
     return output_layer
@@ -78,17 +84,19 @@ def model_fn(features, labels, mode):
 #plt.imshow(dev['train_images'][4])
 #plt.show()
 
-model = tf.estimator.Estimator(model_fn)
+tf.logging.set_verbosity(tf.logging.INFO)
+model = tf.estimator.Estimator(model_fn, model_dir='./model/c')
 dev = load_data()
 input_fn = tf.estimator.inputs.numpy_input_fn(
     x={'images': dev['train_images']}, y = dev['train_labels'],
-    batch_size=batch_size, num_epochs=None, shuffle=True)
+    batch_size=batch_size, num_epochs=10, shuffle=True)
 
 t = model.train(input_fn, steps=num_steps)
 
 input_fn = tf.estimator.inputs.numpy_input_fn(
     x={'images': dev['test_images']}, y = dev['test_labels'],
     batch_size=batch_size, shuffle=False)
-e = model.evaluate(input_fn)
+
+e = model.evaluate(input_fn, steps=num_steps)
 
 print("Testing Accuracy: ", e['accuracy'])
